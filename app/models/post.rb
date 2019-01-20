111b94492ba08bc38a530c53392df6b6002c5478
Ryo1916 <telemon1916@gmail.com>
2019-01-20 20:32:46 +0900

    Generate post model

---
+++ app/models/post.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint(8)        not null, primary key
+#  user_id    :integer
+#  body       :text(65535)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Post < ApplicationRecord
+end
