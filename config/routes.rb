624f9177a4e9c29530075ab935f8f07369be20c3
Ryo1916 <telemon1916@gmail.com>
2019-01-21 02:01:23 +0900

    Implement post button

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  get 'posts/create'
+
   root 'home#index'
 
   devise_for :users
+
+  resources :posts, only: [:create]
 
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   mount Sidekiq::Web, at: '/sidekiq'
